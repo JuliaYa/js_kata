3a167c880976839a995140d9b0e1e66148d6f61b
Julia Yakutova <julia.yakutova@gmail.com>
2020-12-09 16:40:13 +0300

    Fix test for 'Duplicates' kata.

--- tests/duplicates_test.js
+++ tests/duplicates_test.js
@@ -10,7 +10,7 @@ test('If characters not repeats in string should return 0.', t => {
 });
 
 test('Should return number of unic characters that repeats in input string.', t => {
-  t.is(duplicateCount('aabbcde', 2));
+  t.is(duplicateCount('aabbcde'), 2);
   t.is(duplicateCount('aabBcde'), 2);
   t.is(duplicateCount('Indivisibility'), 1);
   t.is(duplicateCount('aA11'), 2);

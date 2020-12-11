21a438fc40c900e73e3925412a539faeeffffe9c
Julia Yakutova <julia.yakutova@gmail.com>
2020-12-11 17:55:31 +0300

    Fix test description.

--- tests/consecutive_nums_test.js
+++ tests/consecutive_nums_test.js
@@ -1,20 +1,20 @@
 import test from 'ava';
 import consecutive from '../src/consecutive_nums.js';
 
-test('If input array is empty should return 0.', t => {
+test('Return 0 for empty array.', t => {
   t.is(consecutive([]), 0);
 });
 
-test('If input array contains one element should return 0.', t => {
+test('Return 0 for array with one element.', t => {
   t.is(consecutive([1]), 0);
 });
 
-test('If input array contains consecutive nums should return 0.', t => {
+test('Return 0 for array with consecutive numbers.', t => {
   t.is(consecutive([1,2,3,4,5]), 0);
   t.is(consecutive([3,4,1,2]), 0);
 });
 
-test('For valid array should return a count of numbers that make the array consecutive.', t => {
+test('Return a count of numbers that make the array consecutive.', t => {
   t.is(consecutive([4,8,6]), 2);
   t.is(consecutive([4,8,6,14,18]), 10);
 });

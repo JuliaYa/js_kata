fe67904c740a30a959c9136df866074665d0c5e4
Julia Yakutova <julia.yakutova@gmail.com>
2020-12-14 17:33:14 +0300

    Improve test readability.

--- tests/same_arrays_test.js
+++ tests/same_arrays_test.js
@@ -16,22 +16,20 @@ test('Return true for empty arrays.', t => {
 })
 
 test('Return true if arrays have the "same" elements, with the same multiplicities.', t => {
-  const a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-  const a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
-  const a4 = [85, 81, 86];
-  const a5 = [7225, 6561, 7396];
-  const a6 = [70, 70];
-  const a7 = [4900, 4900];
-  t.true(comp(a1, a2));
-  t.true(comp(a6, a7));
-  t.true(comp(a4, a5));
+  t.true(
+    comp(
+      [121, 144, 19, 161, 19, 144, 19, 11],
+      [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+  ));
+  t.true(comp([70, 70], [4900, 4900]));
+  t.true(comp([85, 81, 86],[7225, 6561, 7396]));
 });
 
 test('Return false if arrays has not the "same" elements, with the same multiplicities.', t => {
-  const a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-  const a2 = [132, 14641, 20736, 361, 25921, 361, 20736, 361];
-  const a3 = [85, 81, 86];
-  const a4 = [7225, 6551, 7396];
-  t.false(comp(a1, a2));
-  t.false(comp(a3, a4));
+  t.false(
+    comp(
+      [121, 144, 19, 161, 19, 144, 19, 11],
+      [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+  ));
+  t.false(comp([85, 81, 86], [7225, 6551, 7396]));
 });

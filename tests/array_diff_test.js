c489d1d78f243654057507f40ce1a5458854eb81
Julia Yakutova <julia.yakutova@gmail.com>
2020-12-25 20:46:00 +0300

    Correct tests readability.

--- tests/array_diff_test.js
+++ tests/array_diff_test.js
@@ -1,22 +1,30 @@
 import test from 'ava';
 import arrayDiff from '../src/array_diff.js'
 
-test('Return [] for empty arrays.', t => {
-  t.deepEqual(arrayDiff([], []), []);
+test('returns empty when first array is empty', t => {
+  t.deepEqual(
+    arrayDiff([], [4,5]),
+    []
+  );
 });
 
-test('Return [] if first array is empty.', t => {
-  t.deepEqual(arrayDiff([], [4,5]), []);
+test('returns first array when second array is empty', t => {
+  t.deepEqual(
+    arrayDiff([1,8,2], []),
+    [1,8,2]
+  );
 });
 
-test('Return first array if second array is empty.', t => {
-  t.deepEqual(arrayDiff([1,8,2], []), [1,8,2]);
+test('returns an array with difference between first and second arrays', t => {
+  t.deepEqual(
+    arrayDiff([1,2],[1]),
+    [2]
+  );
 });
 
-test('Return an array with difference between first and second arrays.', t => {
-  t.deepEqual(arrayDiff([1,2],[1]), [2]);
-});
-
-test('Return an array contained difference without repeated numbers.', t => {
-  t.deepEqual(arrayDiff([1,2,2,2,3],[2]), [1,3]);
+test('returns an array contained difference without repeated numbers', t => {
+  t.deepEqual(
+    arrayDiff([1,2,2,2,3],[2]),
+    [1,3]
+  );
 });
